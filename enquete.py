3a47483371640201ec60a49beb5977476290f6c1
ol1boni <maluoliboni1028@gmail.com>
2026-09-17 19:32:01 -0300

    .

--- enquete.py
+++ enquete.py
@@ -12,3 +12,17 @@ def listar_opcoes(opcoes):
     print("----- Opções cadastradas -----")
     for i in range(len(opcoes)):
         print(i, "-", opcoes[i])
+
+def registrar_voto(opcoes, votos):
+    if len(opcoes) == 0:
+        print("Não há opções cadastradas para votar.")
+        return
+
+    listar_opcoes(opcoes)
+    posicao = int(input("Digite o número da opção que deseja votar: "))
+
+    if posicao < 0 or posicao >= len(opcoes):
+        print("Opção inválida.")
+    else:
+        votos[posicao] = votos[posicao] + 1
+        print("Voto registrado em:", opcoes[posicao])
